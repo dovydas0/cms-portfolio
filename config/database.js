d488a79af6cec103e7170b20463f99ffb12cdf84
Dovydas0 <mdovydas00@gmail.com>
2024-07-04 16:10:05 +0300

    specifying the client

--- config/database.js
+++ config/database.js
@@ -69,6 +69,7 @@ module.exports = ({ env }) => {
       },
     },
     postgres: {
+      client: "postgres",
       connection: {
         connectionString: env("DATABASE_URL"),
         host: env("DATABASE_HOST", "localhost"),
