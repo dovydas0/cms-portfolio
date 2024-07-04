1c0dad9d9bb994d713185cf42e7886c3c453c559
Dovydas0 <mdovydas00@gmail.com>
2024-07-04 16:28:49 +0300

    changing to postgres

--- config/database.js
+++ config/database.js
@@ -74,9 +74,9 @@ module.exports = ({ env }) => {
         connectionString: env("DATABASE_URL"),
         host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        database: env("DATABASE_NAME", "postgres"),
+        user: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "admin"),
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
@@ -109,7 +109,7 @@ module.exports = ({ env }) => {
 
   return {
     connection: {
-      client,
+      client: "postgres",
       ...connections[client],
       acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
     },
